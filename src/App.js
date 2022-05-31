d04c744f7fa40fa26d84bc192f580e48faa4d866
Vir7uaL <wilson.gomes@innowave.tech>
2022-05-31 21:31:36 +0200

    Variable with html!

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import React from 'react';
 
 function App() {
+    const variabeWithHtml = <h1 className='text-center'>React rules!</h1>;
+
     return (
-        <div class="container">
+        <div className="container">
+            {variabeWithHtml}
             <div class="row">
                 <div class="col">One</div>
                 <div class="col">One</div>
