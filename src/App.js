79b7b05434dc0da7235a912518337ed0a6eedc8b
Vir7uaL <wilson.gomes@innowave.tech>
2022-05-31 21:58:09 +0200

    Component with parameters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import firstComponent from './components/firstComponent/firstComponent';
+import Withparams from './components/withParams/withParams';
 
 function App() {
     const variabeWithHtml = <h1 className='text-center'>React rules!</h1>;
@@ -11,6 +12,10 @@ function App() {
             <div className="row">
                 <div className="col">One</div>
                 <div className="col">One</div>
+            </div>
+
+            <div className='row'>
+                <Withparams name="John Doe"></Withparams>
             </div>
         </div>
     );
---
+++ src/components/withParams/withParams.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Withparams = (props) => {
+    return (
+        <p>Name: {props.name} </p>
+    );
+}
+
+export default Withparams;
