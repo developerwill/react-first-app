98f5ebe4cdc8d0e576ea43e6f44e3c28be84e8d4
Vir7uaL <wilson.gomes@innowave.tech>
2022-05-31 22:14:49 +0200

    Components imports

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import firstComponent from './components/firstComponent/firstComponent';
 import Withparams from './components/withParams/withParams';
+import RandomNumber from './components/randomNumber/randomNumber';
+import Card from './components/Card';
 
 function App() {
     const variabeWithHtml = <h1 className='text-center'>React rules!</h1>;
@@ -17,6 +19,13 @@ function App() {
             <div className='row'>
                 <Withparams name="John Doe"></Withparams>
             </div>
+
+            <div className='row'>
+                <Card title="First Challenge" />
+
+                <RandomNumber min={1} max={10} />
+            </div>
+
         </div>
     );
 }
