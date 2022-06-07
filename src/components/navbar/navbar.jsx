6f999d88feae9d751effe42844be10c58e060b54
Vir7uaL <wilson.gomes@innowave.tech>
2022-06-07 09:54:44 +0200

    Navbar links with react-router-dom

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import navbarLinks from './navbarLinks';
+import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
+
+    const links = navbarLinks.map((data, index) => {
+        return (
+            <li className="nav-item" key={index}>
+                <Link className='nav-link' to={data.href}>
+                    {data.text}
+                </Link>
+            </li>
+        );
+    });
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a href="/" className="navbar-brand">Home</a>
+                <Link className='nav-link' to="/">
+                    Home
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className='nav-link' href="#">
-                            Link
-                        </li>
+                        {links}
                     </ul>
                 </div>
             </div>
---
+++ src/components/navbar/navbarLinks.js
@@ -0,0 +1,44 @@
+const navbarLinks = [
+    {
+        href: '/family',
+        text: 'Family',
+    },
+    {
+        href: '/binding-parent',
+        text: 'Binding Parent',
+    },
+    {
+        href: '/binding-event',
+        text: 'Binding Event',
+    },
+    {
+        href: '/game',
+        text: 'Game',
+    },
+    {
+        href: '/input',
+        text: 'Input OnChange',
+    },
+    {
+        href: '/products-list',
+        text: 'Products List',
+    },
+    {
+        href: '/student-check',
+        text: 'Students Check',
+    },
+    {
+        href: '/students',
+        text: 'Students',
+    },
+    {
+        href: '/with-params',
+        text: 'Component with params',
+    },
+    {
+        href: '/',
+        text: 'home',
+    },
+];
+
+export default navbarLinks;
